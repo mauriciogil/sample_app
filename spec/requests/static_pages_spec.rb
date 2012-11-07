ef8aa11b49b3aad92bfc3f91e7aea7932b7c5c69
Mauricio Gil <mauriciogil@gmail.com>
2012-11-07 14:34:36 +0100

    After Sign up

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
 
   describe "Home page" do
@@ -28,6 +27,20 @@ describe "Static pages" do
     before { visit contact_path }
   	it { should have_selector('h1', :text => 'Contact') }
   	it { should have_selector('title', :text => full_title('Contact')) }
+  end
+
+  describe "Links on layout should go to the right pages" do
+    before { visit root_path }
+    it "should have the right links on the layout" do
+      click_link "Home"
+      page.should have_selector 'title', text: full_title('')
+      click_link "Help"
+      page.should have_selector 'title', text: full_title('Help')
+      click_link "About"
+      page.should have_selector 'title', text: full_title('About')
+      click_link "Contact"
+      page.should have_selector 'title', text: full_title('Contact')
+    end
   end
 
 end
